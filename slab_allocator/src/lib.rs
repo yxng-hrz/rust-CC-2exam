028db1a95f386b0e4d4a7172211283171c60299c
Guillaume Houriez <64561029+yxng-hrz@users.noreply.github.com>
2026-01-19 16:02:55 +0100

    Define constants for slab size and max object size

--- slab_allocator/src/lib.rs
+++ slab_allocator/src/lib.rs
@@ -4,6 +4,9 @@ use core::alloc::{GlobalAlloc, Layout};
 use core::ptr::NonNull;
 use core::mem;
 
+const SLAB_SIZE: usize = 4096;
+const MAX_OBJECT_SIZE: usize = 512;
+
 #[cfg(test)]
 mod tests {
     extern crate std;
