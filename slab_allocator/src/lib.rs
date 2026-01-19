2fe44c6137a6200cebdcbd14554b5ab0cf5f77e9
Guillaume Houriez <64561029+yxng-hrz@users.noreply.github.com>
2026-01-19 16:10:07 +0100

    Add FreeNode struct to manage free nodes

--- slab_allocator/src/lib.rs
+++ slab_allocator/src/lib.rs
@@ -7,6 +7,10 @@ use core::mem;
 const SLAB_SIZE: usize = 4096;
 const MAX_OBJECT_SIZE: usize = 512;
 
+struct FreeNode {
+    next: Option<NonNull<FreeNode>>,
+}
+
 #[cfg(test)]
 mod tests {
     extern crate std;
